9f8d89206266081f340b08ee236904090b38e7e2
Emirhan <Demirci.Emirhan@outlook.com>
2020-04-01 21:24:38 +0200

    api url
    api url

--- src/app/shared/user.service.ts
+++ src/app/shared/user.service.ts
@@ -8,7 +8,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 export class UserService {
 
   constructor(private fb: FormBuilder, private http: HttpClient) { }
-  readonly BaseURI = 'https://localhost:44358/api';
+  readonly BaseURI = 'http://fdemirci.nl:7575/api';
 
   formModel = this.fb.group({
     UserName: ['', Validators.required],
