f58e36c022de31ee748127e3d172341e62c03da1
Emirhan <Demirci.Emirhan@outlook.com>
2020-04-02 11:30:25 +0200

    Rooster page added

--- src/app/rooster-page/rooster-page.component.ts
+++ src/app/rooster-page/rooster-page.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-rooster-page',
   templateUrl: './rooster-page.component.html',
-  styleUrls: ['./rooster-page.component.css']
+  styles: []
 })
 export class RoosterPageComponent implements OnInit {
 
