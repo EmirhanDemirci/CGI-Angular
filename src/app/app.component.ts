98e60ef8545bd18a0d294d0dd0cc8728d98440d7
Emirhan <Demirci.Emirhan@outlook.com>
2020-04-23 11:28:48 +0200

    Logout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
+import { UserService } from '../../src/app/shared/user.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  userDetails;
+  constructor(public router: Router,private service:UserService) { }
   title = 'CGI-Angular';
+
+  ngOnInit() {
+    this.service.getUserProfile().subscribe(
+      res =>{ 
+        this.userDetails = res;
+      },
+      err =>{
+        console.log(err);
+      },
+    );
+  }
+  onLogout(){
+    // Delete the token (Logout)
+    localStorage.removeItem('token');
+    this.router.navigate(['/user/login'])
+  }
 }
