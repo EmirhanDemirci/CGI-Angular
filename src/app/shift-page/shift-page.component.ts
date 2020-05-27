f22b58151e05626de619d7f10aaba005f9010d75
boaz <boazgoldsteen>
2020-05-27 10:14:03 +0200

    Sweetalert
    Added sweetalert ft Emirhan

--- src/app/shift-page/shift-page.component.ts
+++ src/app/shift-page/shift-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-shift-page',
@@ -10,6 +11,16 @@ export class ShiftPageComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    
+  }
+
+  CallSweetalert(){
+    Swal.fire({
+      title: 'Error!',
+      text: 'Do you want to continue',
+      icon: 'error',
+      confirmButtonText: 'Cool'
+    })
   }
 
 }
