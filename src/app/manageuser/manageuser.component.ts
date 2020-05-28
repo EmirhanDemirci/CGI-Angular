5ad04e70b10c33ab0957946e7814c058cd6259fc
fortress <david.la.gordt.dillie@gmail.com>
2020-05-28 09:24:16 +0200

    manager user page done

---
+++ src/app/manageuser/manageuser.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-manageuser',
+  templateUrl: './manageuser.component.html',
+  styles: []
+})
+export class ManageuserComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
